1a13ab3e780b362b111297a27a9f32dd65952645
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-30 11:35:48 +0530

    feat(server/routes): add auth routes for user registration and login in index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import postRoutes from "./routes/postRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -28,6 +29,9 @@ app.get("/api/status", (req, res) => {
 
 // importing post routes
 app.use("/api/posts", postRoutes);
+
+// importing auth routes
+app.use("/api/auth", authRoutes);
 
 // Connect to database
 connectDB();
