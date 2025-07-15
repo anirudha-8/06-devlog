bea63eea21cf4a61cf3a651e7e383960255935fb
Anirudha Bele <beleanirudha8@gmail.com>
2025-07-15 09:54:36 +0530

    feat(server/config/db.js): add JWT_SECRET validation during database initialization

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 	try {
 		const db_uri = process.env.MONGODB_URI;
+		const jwt_secret = process.env.JWT_SECRET;
+
 		if (!db_uri) throw new Error("MONGODB_URI not found in environment");
+		if (!jwt_secret) throw new Error("JWT_SECRET not found in environment");
 
 		const connect = await mongoose.connect(db_uri);
 		console.log(`✅ MongoDB connected: ${connect.connection.name}`);
