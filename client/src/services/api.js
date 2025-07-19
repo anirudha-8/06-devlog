598ba34dd826aaf7ccc8005f89156ee1bb07800e
Anirudha Bele <beleanirudha8@gmail.com>
2025-07-19 10:34:21 +0530

    feat(client/src/services/api.js): add JWT token interceptor to API requests

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 
 const API = axios.create({
-	baseURL: "http://localhost:8080/api", // adjust if using a different port
+	baseURL: "http://localhost:8080/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
 });
+
+// Add token to request headers if it exists
+API.interceptors.request.use(
+	(config) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
 
 export default API;
